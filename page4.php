46c3ab0f0855f63cdb21b326a5a7f573015f070f
dageyu <cuiyu1478@163.com>
2019-02-19 16:09:22 +0800

    2.19.16:09

--- page4.php
+++ page4.php
@@ -5,5 +5,16 @@ echo "PHP软件版本:".phpversion();
 // echo mysql_get_server_info();
 echo apache_get_version();
 
+/**
+ * 返回当前集合中指定位置的元素
+ * @method
+ * @for NodeList
+ * @param {Number} [i=0] 位置下标。如果为负数，则从集合的最后一个元素开始倒数
+ * @return {Element} 指定元素
+ */
+
+
+
+
 
 ?>
