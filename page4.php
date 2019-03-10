ec8f60dd92ad042425ab712216e7db277b6d1191
dageyu <cuiyu1478@163.com>
2019-03-10 21:02:22 +0800

    增加IIS

--- page4.php
+++ page4.php
@@ -1,9 +1,3 @@
 <?php
-namespace study;
-const PATH = 'abcd';
-echo namespace\PATH;
-// session_start();
-// echo session_id();
-// var_dump($_COOKIE);//to36s984qutvrri7bu81vnh7p5
-echo __NAMESPACE__;
+PHPinfo();
 ?>
