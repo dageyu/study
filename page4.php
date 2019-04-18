821cb1d9f3c70f7325b12e993e6f26e53de75821
dageyu <cuiyu1478@163.com>
2019-04-18 17:53:52 +0800

    语言选项配置

--- page4.php
+++ page4.php
@@ -1,7 +1,19 @@
 <?php
 header('Content-type:text/html;charset=utf8');
-// $str = 1.566666;
-// echo $str;
-echo time();
+$serialized_object='O:1:"a":1:{s:5:"value";s:3:"100";}';
+
+ini_set('unserialize_callback_func','mycallback');
+
+function mycallback($classname) {
+    echo 1;
+}
+
+function func2() 
+{
+    echo 2;
+}
+
+spl_autoload_register('func2');
+unserialize($serialized_object);
 
 ?>
